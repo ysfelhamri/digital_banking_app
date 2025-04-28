655d6747df451e594890f955659f74511789557b
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-04-28 09:16:03 +0100

    Initial commit

---
+++ src/main/java/q/jv/digital_banking_app/DigitalBankingAppApplication.java
@@ -0,0 +1,13 @@
+package q.jv.digital_banking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/q/jv/digital_banking_app/DigitalBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package q.jv.digital_banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
