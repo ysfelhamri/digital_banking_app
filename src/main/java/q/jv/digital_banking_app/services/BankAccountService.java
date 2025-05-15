f75a3de2de7fd9f10113216a1b976752f252de16
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-15 22:31:45 +0100

    Added BankAccountService interface

---
+++ src/main/java/q/jv/digital_banking_app/services/BankAccountService.java
@@ -0,0 +1,31 @@
+package q.jv.digital_banking_app.services;
+
+import q.jv.digital_banking_app.dtos.BankAccountDTO;
+import q.jv.digital_banking_app.dtos.CurrentBankAccountDTO;
+import q.jv.digital_banking_app.dtos.CustomerDTO;
+import q.jv.digital_banking_app.dtos.SavingBankAccountDTO;
+import q.jv.digital_banking_app.exceptions.BalanceNotSufficientException;
+import q.jv.digital_banking_app.exceptions.BankAccountNotFoundException;
+import q.jv.digital_banking_app.exceptions.CustomerNotFoundException;
+
+import java.util.List;
+
+public interface BankAccountService {
+    CustomerDTO saveCustomer(CustomerDTO customerDTO);
+    CurrentBankAccountDTO saveCurrentBankAccount(double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+    SavingBankAccountDTO saveSavingBankAccount(double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
+    List<CustomerDTO> listCustomers();
+    BankAccountDTO getBankAccount(String accountId) throws BankAccountNotFoundException;
+    void debit(String accountId, double amount, String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
+    void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
+    void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
+
+    List<BankAccountDTO> bankAccountList();
+
+    CustomerDTO getCustomer(Long customerId) throws CustomerNotFoundException;
+
+    CustomerDTO updateCustomer(CustomerDTO customerDTO);
+
+    void deleteCustomer(Long customerId);
+    List<CustomerDTO> searchCustomers(String keyword);
+}
