c0150ed54b4df6456f15e32f1bf1c1ee96676bba
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-15 22:24:15 +0100

    Added BankAccountDTO and the DTOs that inherit from it, CurrentBankAccountDTO and SavingBankAccountDTO

---
+++ src/main/java/q/jv/digital_banking_app/dtos/BankAccountDTO.java
@@ -0,0 +1,8 @@
+package q.jv.digital_banking_app.dtos;
+
+import lombok.Data;
+
+@Data
+public class BankAccountDTO {
+    private String type;
+}
---
+++ src/main/java/q/jv/digital_banking_app/dtos/SavingBankAccountDTO.java
@@ -0,0 +1,16 @@
+package q.jv.digital_banking_app.dtos;
+
+import lombok.Data;
+import q.jv.digital_banking_app.enums.AccountStatus;
+
+import java.util.Date;
+
+@Data
+public class SavingBankAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double interestRate;
+}
